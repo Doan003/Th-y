ee140cdbf97e607c57bb7fee03ddd8aa6988618a
Doan003 <dtc21h4802010108@ictu.edu.vn>
2025-07-19 21:58:40 +0700

    Cập nhật nội dung sau chỉnh sửa

--- Source/api/index.php
+++ Source/api/index.php
@@ -1,10 +1,10 @@
 <?php
-<?php
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 echo "Trang đang hoạt động<br>";
 
-require("../header.php");
-require("../mainmenu.php");
-require("../footer.php");
+//require("../header.php");
+//require("../mainmenu.php");
+//require("../footer.php");
 ?>
